2553c394ada7bd0c3cbc472cd05da8e9812fa8be
gatordevin <gatordevin@gmail.com>
2019-10-25 17:43:54 -0400

    HTML image extraction working

--- DownloadImages.py
+++ DownloadImages.py
@@ -5,6 +5,10 @@ import re
 import wget
 import requests
 import shutil
+from PIL import Image
+from PIL import ImageFont
+from PIL import ImageDraw 
+import textwrap
 
 url = "https://www.mdpi.com/1420-3049/24/20/3682/htm"
 spliturl = url.split('/', 3)
@@ -13,9 +17,20 @@ req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
 html_page = urlopen(req).read()
 soup = BeautifulSoup(html_page)
 images = []
+captions = []
 
 for img in soup.findAll('img'):
-    images.append(img.get('src'))
+    if(img.parent.name == "div"):
+        try:
+            captions.append(img.parent.parent.parent.find("div",{"class": "html-fig_description"}).text)
+        except:
+            captions.append("")
+        images.append(img.get('src'))
+
+mydivs = soup.findAll("div", {"class": "html-fig_description"})
+for div in mydivs:
+    #print(div)
+    None
 
 count = 0
 for img in images:
@@ -28,4 +43,11 @@ for img in images:
             with open(str(count) + ".png", 'wb') as f:
                 r.raw.decode_content = True
                 shutil.copyfileobj(r.raw, f)
+            img = Image.open(str(count) + ".png")
+            draw = ImageDraw.Draw(img)
+            margin = offset = 20
+            for line in textwrap.wrap(str(captions[count-1].encode("utf-8").decode('latin-1')), width=img.width/7):
+                draw.text((margin, offset), line)
+                offset += 10
+            img.save(str(count) + ".png")
 
